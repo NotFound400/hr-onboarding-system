7f9d8276bd2d9d84d4b85b6a75a5a6528b5e6030
tengy <1270122172@qq.com>
2025-12-05 15:05:17 -0500

    Add services module

---
+++ housing-service/src/test/java/org/example/housingservice/HousingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.housingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
