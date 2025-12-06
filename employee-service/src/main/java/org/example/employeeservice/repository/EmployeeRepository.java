67c563fa41ce9282754fd0a23f3d3601cddd2dd8
joeywangdeveloper <78221917+joeywangdeveloper@users.noreply.github.com>
2025-12-06 14:58:20 -0600

    Create init features

---
+++ employee-service/src/main/java/org/example/employeeservice/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package org.example.employeeservice.repository;
+
+import org.example.employeeservice.model.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
+}
