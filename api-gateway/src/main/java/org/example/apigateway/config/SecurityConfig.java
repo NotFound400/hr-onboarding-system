f763417659dcbc2b086ba79bb9cf609205db1a7b
tengy <1270122172@qq.com>
2025-12-12 08:48:39 -0500

    Minor Fix

--- api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.example.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -22,6 +23,7 @@ public class SecurityConfig {
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyExchange().permitAll()
                 )
                 .build();
